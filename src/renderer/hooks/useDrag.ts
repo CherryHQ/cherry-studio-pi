ffda75432190360bd7120e8493af323525bfc148
yinsen <cherryai001@yinsenMac-mini.local>
2026-06-20 11:32:28 +0800

    fix(hooks): guard drag leave related target

--- src/renderer/hooks/useDrag.ts
+++ src/renderer/hooks/useDrag.ts
@@ -23,7 +23,8 @@ export const useDrag = <T extends HTMLElement>(onDrop?: (e: React.DragEvent<T>) 
     e.stopPropagation()
     // 确保是离开当前元素，而不是进入子元素
     // logger.debug('drag leave', { currentTarget: e.currentTarget, relatedTarget: e.relatedTarget })
-    if (e.currentTarget.contains(e.relatedTarget as Node)) {
+    const relatedTarget = e.relatedTarget
+    if (relatedTarget instanceof Node && e.currentTarget.contains(relatedTarget)) {
       return
     }
     setIsDragging(false)
